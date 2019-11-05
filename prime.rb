9a63ad1d90f80986fb6e04a0c1293a8df61fdcf7
Michael Calkins <m.calkins576@gmail.com>
2019-11-05 12:35:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,8 +4,9 @@ require 'pry'
 def prime?(int)
   primes = []
   numbers = [1..2_000_000]
-  numbers.map do |i|
-    primes << i if i%i == 0 && i%1 == 0
+  numbers.each do |i|
+    if i%i == 0 && i%1 == 0
+    primes << i 
   end
   binding.pry
   if primes.include?(int)
