9327fdf5efaea84d8e2fc34f6ced053a1a43312d
Michael Calkins <m.calkins576@gmail.com>
2019-11-05 13:12:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,6 +4,10 @@ require 'pry'
 def prime?(int)
   numbers = (1..2_000_000).to_a
   numbers.each do |i|
-    if int % i 
+    if int % i == 0 && i < int
+      false
+    else 
+      true
+    end
   end
 end
