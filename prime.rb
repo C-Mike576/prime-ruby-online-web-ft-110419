c4545e4cb5c547cb0d0bc604b98b87fe63659c0f
Michael Calkins <m.calkins576@gmail.com>
2019-11-05 12:35:30 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,8 +5,7 @@ def prime?(int)
   primes = []
   numbers = [1..2_000_000]
   numbers.each do |i|
-    
-    primes << i 
+    primes << i if i%i == 0 && i%1 == 0
   end
   binding.pry
   if primes.include?(int)
