fe6512f655d6dd707c66ea98da25fdb2a5b3b912
Michael Calkins <m.calkins576@gmail.com>
2019-11-05 12:32:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,12 @@
 # Add  code here!
+require 'pry'
+
 def prime?(int)
   numbers = [1..2_000_000]
   numbers.map do |i|
     i if i%i == 0 && i%1 == 0
   end
+  binding.pry
   if numbers.include?(int)
     true
   else
