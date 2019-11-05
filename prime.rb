39e0fdb80a3d95d347704ff8dbadfc17a33726be
Michael Calkins <m.calkins576@gmail.com>
2019-11-05 13:04:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,7 +6,7 @@ def prime?(int)
   numbers = (1..2_000_000).to_a
   
   numbers.each do |i|
-    binding.pry
+    #binding.pry
     primes << i if i%i == 0 && i%1 == 0 && !(i%2 == 0)
   end
   primes.unshift(2)
