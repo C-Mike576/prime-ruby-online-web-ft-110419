7b1c0af54ccf14f244c5df64c14e2abbe5c883a7
Michael Calkins <m.calkins576@gmail.com>
2019-11-05 12:31:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,12 @@
 # Add  code here!
 def prime?(int)
+  numbers = [1..2_000_000]
+  numbers.map do |i|
+    i if i%i == 0 && i%1 == 0
+  end
+  if numbers.include?(int)
+    true
+  else
+    false
+  end
 end
